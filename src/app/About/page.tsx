e08940f08c5dc1b407b9446de4ec46a482241436
sorincetean <72597288+sorincetean@users.noreply.github.com>
2024-07-29 21:36:45 +0300

    added about page

---
+++ src/app/About/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import styles from "./page.module.css";
+import Header from "../Components/header";
+
+const HomePage: React.FC = () => {
+    return (
+      <div className={styles.centeredContent} >
+        <h1>Welcome to the About page</h1>
+      </div>
+    );
+  };
+  
+  export default HomePage;
+  
--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -5,17 +5,15 @@ import styles from "./header.module.css"; // Adjust path as needed
 const Header: React.FC = () => {
   const router = useRouter();
 
-  const navigate = (path: string) => {
-    router.push(path);
-  };
-
+  const navigate = (path: string) => {  router.push(path);  };
+   
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} ${styles.navLink}`}>
         <button onClick={() => navigate('/')}>Home</button>
-        <button onClick={() => navigate('/about')}>About</button>
-        <button onClick={() => navigate('/pricing')}>Pricing</button>
-        <button onClick={() => navigate('/signin')}>Sign In</button>
+        <button onClick={() =>navigate('/About')}>About</button>
+        <button onClick={() => navigate('/Pricing')}>Pricing</button>
+        <button onClick={() => navigate('/SignIn')}>Sign In</button>
       </nav>
     </header>
   );
